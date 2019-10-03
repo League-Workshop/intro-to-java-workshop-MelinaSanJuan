eaf2d3818fe8f1d86da156d7dada280448cd2ad4
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-10-03 13:28:49 -0700

    work shop robot

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -6,19 +6,28 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// 2. Create a new Robot
-
+	Robot marie=new Robot();
 	void go() {
 		// 4. Make the robot move as fast as possible
+		marie.setSpeed(200);
 
 		// 5. Set the pen width to 5
+		marie.setPenWidth(5);
 
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
-
+		for(int i=0; i<4;i++) {
 			// 7. Set the pen color to random
-	
+		   marie.setRandomPenColor();
 			// 1. Call the drawSquare() method
+		
+		drawSquare();
+		
+		
+		
 	
 			// 8. Turn the robot 90 degrees to the right
+		marie.turn(90);
+		}
 
 	}
 
@@ -27,7 +36,10 @@ public class FourSquare {
 		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
 		/* 3. Fill in the code to draw a square inside the method below. */
 		
-		
+		for(int i=0; i<4;i++) {
+			marie.move(200);	
+			marie.turn(90);
+			}
 	}
 
 	public static void main(String[] args) {
--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,13 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+	Robot rose=new Robot();
+	rose.setSpeed(100);
+	rose.penDown();
+	for(int i=0; i<4;i++) {
+	rose.move(200);	
+	rose.turn(90);
+	}
 	
-		
-		
 	}
 }
