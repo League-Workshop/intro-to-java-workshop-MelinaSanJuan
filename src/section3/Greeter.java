4c332cab2d8c0b91c797c107460e73d0f55df104
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-10-17 13:26:20 -0700

    Dragon Fight

--- src/section3/Greeter.java
+++ src/section3/Greeter.java
@@ -6,7 +6,7 @@ public class Greeter {
 public static void main(String[] args) {
 String name =JOptionPane.showInputDialog("What is your name?");
 System.out.println(name);
-JOptionPane.showMessageDialog(null, "Good afternoon " +name);
+JOptionPane.showMessageDialog(null, "Good Afternoon " +name);
 
 
 }
